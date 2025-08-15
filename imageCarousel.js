f141e7e96e17f4520223cda01ebc1fa787715808
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2025-08-15 18:31:07 -0400

    set up circle buttons lined up and centered watch slides and btns

--- imageCarousel.js
+++ imageCarousel.js
@@ -1,13 +1,34 @@
 //handles logic taking first li and appending to end
-function rotateCarosel () {
+function rotateCaroselLeft () {
     const watches = document.querySelector("#watches")
     const watchList = document.getElementsByTagName("li");
     watches.appendChild(watchList[0])
 }
 
-//rotates carosel at intervals of 5 seconds
-//const interValID = setInterval(rotateCarosel,5000)
+//handles logic taking first li and appending to end
+function rotateCaroselRight () {
+    const watches = document.querySelector("#watches")
+    const watchList = document.getElementsByTagName("li");
+    watches.prepend(watchList[4])
+    
+
+}
 
 
 
 
+
+
+function caroselControls () {
+    //rotates carosel at intervals of 5 seconds
+    const caroselTimer = setInterval(rotateCaroselLeft,5000)
+
+    const previous = document.querySelector(".previous");
+    previous.addEventListener("click", rotateCaroselLeft);
+
+    const nextSlide = document.querySelector(".next");
+    nextSlide.addEventListener("click", rotateCaroselRight);
+}
+
+
+caroselControls();
