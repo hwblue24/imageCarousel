bc3297d58b025c8c973aeaa1a08c10e0f83deb4c
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2025-08-14 21:47:45 -0400

    set up carosel function to rotate 1st element to end

---
+++ imageCarousel.js
@@ -0,0 +1,18 @@
+function rotateCarosel () {
+    const watches = document.querySelector("#watches")
+    const watchList = document.getElementsByTagName("li");
+    watches.appendChild(watchList[0])
+}
+
+
+
+function timeCarosel () {
+    while true {
+        //run rotateCarous()
+    //wait 5 seconds 
+
+    }
+    
+}
+
+
