2980ecf93f382d5b8eed7c565d31b5a73477eaab
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2025-08-15 15:01:34 -0400

    set up interval method to rotate carosel and set up arrows and initalize navigation bar

--- imageCarousel.js
+++ imageCarousel.js
@@ -1,18 +1,13 @@
+//handles logic taking first li and appending to end
 function rotateCarosel () {
     const watches = document.querySelector("#watches")
     const watchList = document.getElementsByTagName("li");
     watches.appendChild(watchList[0])
 }
 
+//rotates carosel at intervals of 5 seconds
+//const interValID = setInterval(rotateCarosel,5000)
 
 
-function timeCarosel () {
-    while true {
-        //run rotateCarous()
-    //wait 5 seconds 
-
-    }
-    
-}
 
 
